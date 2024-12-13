e4a5cd96bfe3e0e4baeb0aab3045dc95a9ddd22a
2U1 <kirito01@naver.com>
2024-12-13 16:41:10 +0900

    fix: handle None state_dict in Phi3VTrainer save method

--- src/training/trainer.py
+++ src/training/trainer.py
@@ -203,6 +203,8 @@ class Phi3VTrainer(Trainer):
                     else:
                         torch.save(state_dict, os.path.join(output_dir, WEIGHTS_NAME))
             else:
+                if state_dict is None:
+                    state_dict = self.model.state_dict()
                 state_dict = {k:v for k, v in state_dict.items() if "wte" not in k}
                 self.model.save_pretrained(
                     output_dir, state_dict=state_dict, safe_serialization=self.args.save_safetensors
